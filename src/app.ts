6427fc3b2063ef7f6d74a5f9e29c0318e474c5a8
borgerry <borodingerry@gmail.com>
2022-05-20 16:33:40 +0300

    Draft version

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,32 @@ import { Context, Markup, Telegraf, Telegram } from 'telegraf';
 import { Update } from 'typegram';
 
 const token: string = process.env.BOT_TOKEN as string;
-
 const telegram: Telegram = new Telegram(token);
-
 const bot: Telegraf<Context<Update>> = new Telegraf(token);
-
 const chatId: string = process.env.CHAT_ID as string;
 
 bot.start((ctx) => {
-  ctx.reply('Hi ' + ctx.from.first_name + '!');
+  ctx.reply('Вітаю, ' + ctx.from.first_name + '!');
 });
 
 bot.help((ctx) => {
-  ctx.reply('Send /start to receive a greeting');
-  ctx.reply('Send /keyboard to receive a message with a keyboard');
-  ctx.reply('Send /quit to stop the bot');
+  ctx.reply('Введіть /search для визначення вашого міста');
+  ctx.reply('Введіть /quit для зупинки бота');
+});
+
+bot.command('start', (ctx) => {
+  ctx.reply(
+    'Визначитssи геолокацію',
+    Markup.keyboard([
+      ['🔍 Шукати', '☸ Налаштування'], // Row1 with 2 buttons
+      [ '📞 Контакти', '⭐️ Залишити відгук'], // Row2 with 2 buttons
+      ['📢 Допомога', '👥 Росказати про нас'] // Row3 with 3 buttons
+    ])
+  );
+});
+
+bot.hears('🔍 Шукати', ctx => {
+  ctx.reply('Запит успішно оброблено')
 });
 
 bot.command('quit', (ctx) => {
@@ -25,31 +36,6 @@ bot.command('quit', (ctx) => {
 
   // Context shortcut
   ctx.leaveChat();
-});
-
-bot.command('keyboard', (ctx) => {
-  ctx.reply(
-    'Keyboard',
-    Markup.inlineKeyboard([
-      Markup.button.callback('First option', 'first'),
-      Markup.button.callback('Second option', 'second'),
-    ])
-  );
-});
-
-bot.on('text', (ctx) => {
-  ctx.reply(
-    'You choose the ' +
-      (ctx.message.text === 'first' ? 'First' : 'Second') +
-      ' Option!'
-  );
-
-  if (chatId) {
-    telegram.sendMessage(
-      chatId,
-      'This message was sent without your interaction!'
-    );
-  }
 });
 
 bot.launch();
